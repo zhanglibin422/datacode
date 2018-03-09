895693679fa5a18dc28858ea76e9dfb71434ab56
zhanglibin422 <35481688+zhanglibin422@users.noreply.github.com>
2018-03-10 00:07:07 +0800

    Add files via upload

---
+++ sceo-eureka-server/src/main/java/com/huntkey/rx/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.huntkey.rx.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by zhanglb on 2017/6/6.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
